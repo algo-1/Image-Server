8bc38acf32c1328952d7e58fa3e2fe301248bb6e
Harold Karibiye <haroldkaribiye@gmail.com>
2023-11-21 13:19:14 +0000

    update webpage

--- app/Main.hs
+++ app/Main.hs
@@ -13,11 +13,11 @@ import Shapes
 
 
 drawing :: Drawing
-drawing = [( scale (point 0.5 0.5) <+>shearY (-0.5), square (225, 225, 225)), (identity, hexagon (200, 100, 200))]
+drawing = [( scale (point 0.5 0.5) <+>shearY (-0.5), square (225, 225, 225)), (identity, hexagon (200, 100, 200)), (identity, ellipse (120, 30, 5))]
 
 
 m :: Mask
-m = (translate (point 1.2 0.4), square (255, 255, 255))
+m = (scale (point 0.65 0.65) <+> translate (point 1.2 0.4), rectangle (255, 255, 255))
 
 maskedDrawing:: Either Drawing MaskedDrawing
 maskedDrawing = Right $ mask m drawing
@@ -29,24 +29,20 @@ polygonsDrawing = [(scale (point 0.4 0.4), triangle (200, 150, 20)), (scale (poi
 main :: IO ()
 main =
   do
-    -- print $ show $ nPolygon 3
+    render "the_mask.png" defaultWindow $ Left [m]
     render "non_masked.png" defaultWindow $ Left drawing
     render "masked.png" defaultWindow maskedDrawing
-    render "shear_transform.png" defaultWindow maskedDrawing
-    render "covered_shapes_optimisation.png" defaultWindow maskedDrawing
     render "polygons.png" defaultWindow $ Left polygonsDrawing
-    render "ellipse.png" defaultWindow maskedDrawing
-    render "rectangle.png" defaultWindow maskedDrawing
+
 
     scotty 3000 $ do
       get "/" $ do
-        html $ response [Left polygonsDrawing, Left drawing, maskedDrawing ]
+        html $ response [Left polygonsDrawing, Left drawing, Left [m], maskedDrawing]
 
       get "/images/:file" $ do 
         filename <- param "file"
         file filename
 
--- TODO: make it take a list of drawings and map ??
 response :: [Either Drawing MaskedDrawing]-> Text
 response dslProgram =
   R.renderHtml  $
@@ -59,13 +55,17 @@ response dslProgram =
             H.pre $ H.toHtml $ show $ dslProgram !! 0
             H.img H.! A.src "/images/polygons.png" 
 
-            H.h2 "Non-masked Image"
+            H.h2 "Sheared Square"
             H.h4 "Source Code"
             H.pre $ H.toHtml $ show $ dslProgram !! 1
             H.img H.! A.src "/images/non_masked.png"
             
+            H.h2 "Mask"
+            H.h4 "Source Code"
+            H.pre $ H.toHtml $ show $ dslProgram !! 2
+            H.img H.! A.src "/images/the_mask.png"
+
             H.h2 "Masked Image"      
             H.h4 "Source Code"
-            H.pre $ H.toHtml $ show $ dslProgram !! 2       
+            H.pre $ H.toHtml $ show $ dslProgram !! 3       
             H.img H.! A.src "/images/masked.png"
-
